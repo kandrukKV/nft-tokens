6c8725d8bfbfedb7eaacbf70755411f353cc35e3
Kandruk Konstantin <kandruk@ya.ru>
2022-01-31 22:39:48 +0700

    frtst commit

---
+++ hardhat.config.ts
@@ -0,0 +1,44 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_URL || "",
+      accounts:
+        process.env.PRIVATE_ACC_KEY1 !== undefined &&
+        process.env.PRIVATE_ACC_KEY2 !== undefined
+          ? [process.env.PRIVATE_ACC_KEY1, process.env.PRIVATE_ACC_KEY2]
+          : [],
+      gas: "auto",
+      gasPrice: 30_000_000_000, // gwei
+    },
+    hardhat: {
+      chainId: 31337,
+    },
+  },
+};
+
+export default config;
